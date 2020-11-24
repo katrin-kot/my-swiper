5fab08107ba233b0a9fad7cbcde6827286cf6abb
katrin-kot <enkor89@gmail.com>
2020-11-24 12:53:59 +0300

    add: basic structure

---
+++ src/component/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import styles from './App.module.css';
+class MyComponent extends Component {
+  render() {
+    return <div className={styles.intro}>Hello World</div>;
+  }
+}
+export default MyComponent;
